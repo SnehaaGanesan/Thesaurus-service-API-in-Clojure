28b84c78a5436183a72394a19316441d0f9eee3b
Snehaa Ganesan <sg4860@nyu.edu>
2019-02-14 11:09:42 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,14 +1,10 @@
 (defproject thesapi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-jetty-adapter "1.5.1"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/tools.reader "1.3.2"]
-                 ;;[clojure.string]
                  [compojure "1.6.1"]
-                 ;;[clojure.java.io]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.4"]]
   :main thesapi.core
